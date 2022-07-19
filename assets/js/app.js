babe8e74c08317fd6207213a81d83b4416efdd4e
MurituMaina <vincent.maina@student.moringaschool.com>
2022-07-19 20:01:37 +0300

    Update the changes and images pie 1 and pie 2

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Data Page Below
+function progressUpdate() {
+    var progress = document.getElementById("form");
+    if (progress.style.display === "none") {
+        progress.style.display = "block";
+    } else {
+        progress.style.display = "none";
+    }
+}
+
+//Data Page above
